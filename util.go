50a5e0cfec26bca205b59c8d470c51a9d82f49e9
ekantar <ekantar@ISTTURNB117>
2023-06-22 00:22:36 +0300

    new version v0.1.0

--- util.go
+++ util.go
@@ -10,16 +10,49 @@ type MathUtil struct {
 	LoggingEnabled bool
 }
 
+type Add interface {
+	Add(a int, b int) int
+}
+
+type Subtract interface {
+	Subtract(a int, b int) int
+}
+
+type Multiply interface {
+	Multiply(a int, b int) int
+}
+
 func (mathUtil *MathUtil) Add(a int, b int) int {
 	if mathUtil.LoggingEnabled {
 		fmt.Println("[Add operation started.]")
 	}
-	return a + b
+	res := a + b
+
+	if mathUtil.LoggingEnabled {
+		fmt.Println("[Add operation ended.]")
+	}
+
+	return res
 }
 
 func (mathUtil *MathUtil) Subtract(a int, b int) int {
 	if mathUtil.LoggingEnabled {
 		fmt.Println("[Subtract operation started.]")
 	}
-	return a - b
+	res := a - b
+	if mathUtil.LoggingEnabled {
+		fmt.Println("[Subtract operation ended.]")
+	}
+	return res
+}
+
+func (mathUtil *MathUtil) Multiply(a int, b int) int {
+	if mathUtil.LoggingEnabled {
+		fmt.Println("[Multiply operation started.]")
+	}
+	res := a * b
+	if mathUtil.LoggingEnabled {
+		fmt.Println("[Multiply operation ended.]")
+	}
+	return res
 }
