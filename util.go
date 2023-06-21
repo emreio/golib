743be5718088b286845bf41d5d08926cfeb916af
Emre Kantar <emrekantar87@gmail.com>
2023-06-21 23:25:18 +0300

    Update util.go

--- util.go
+++ util.go
@@ -3,19 +3,19 @@ package golib
 const PACKAGENAME = "golib/PACKAGENAME"
 
 type MathUtil struct {
-	// loggingEnabled bool
+   loggingEnabled bool
 }
 
 func (mathUtil *MathUtil) Add(a int, b int) int {
-	// if mathUtil.loggingEnabled {
-	// 	fmt.Println("[Add operation started.]")
-	// }
+	 if mathUtil.loggingEnabled {
+	 	fmt.Println("[Add operation started.]")
+	 }
 	return a + b
 }
 
 func (mathUtil *MathUtil) Subtract(a int, b int) int {
-	// if mathUtil.loggingEnabled {
-	// 	fmt.Println("[Subtract operation started.]")
-	// }
+	 if mathUtil.loggingEnabled {
+		fmt.Println("[Subtract operation started.]")
+	 }
 	return a - b
 }
