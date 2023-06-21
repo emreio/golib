02afa2e7a2879694c5f9e1f641045691bd00d5a2
ekantar <ekantar@ISTTURNB117>
2023-06-22 00:31:40 +0300

    new version v0.1.1-beta

--- util.go
+++ util.go
@@ -22,6 +22,10 @@ type Multiply interface {
 	Multiply(a int, b int) int
 }
 
+type Divide interface {
+	Divide(a int, b int) (int, error)
+}
+
 func (mathUtil *MathUtil) Add(a int, b int) int {
 	if mathUtil.LoggingEnabled {
 		fmt.Println("[Add operation started.]")
@@ -55,4 +59,8 @@ func (mathUtil *MathUtil) Multiply(a int, b int) int {
 		fmt.Println("[Multiply operation ended.]")
 	}
 	return res
+}
+
+func (mathUtil *MathUtil) Divide(a int, b int) (int, error) {
+	return 1, nil
 }
