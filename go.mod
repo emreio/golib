a1f7dd8ad5a105c2c1d55d9ef12c747b0f63bc21
ekantar <ekantar@ISTTURNB117>
2023-06-21 22:43:58 +0300

    first commit - module beta version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/emreio/golib/v0.0.1-beta1
+
+go 1.19
---
+++ util.go
@@ -0,0 +1,21 @@
+package golib
+
+const PACKAGENAME = "golib/PACKAGENAME"
+
+type MathUtil struct {
+	// loggingEnabled bool
+}
+
+func (mathUtil *MathUtil) Add(a int, b int) int {
+	// if mathUtil.loggingEnabled {
+	// 	fmt.Println("[Add operation started.]")
+	// }
+	return a + b
+}
+
+func (mathUtil *MathUtil) Subtract(a int, b int) int {
+	// if mathUtil.loggingEnabled {
+	// 	fmt.Println("[Subtract operation started.]")
+	// }
+	return a - b
+}
