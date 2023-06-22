5890650b528e95d2f8724bfcb58b27c4b306f7a1
ekantar <ekantar@ISTTURNB117>
2023-06-22 12:48:47 +0300

    GitHub Actions test

--- tests/dummy_test.go
+++ tests/dummy_test.go
@@ -2,6 +2,8 @@ package tests
 
 import "testing"
 
+//fill this code file with some dummy tests
+
 func TestDummyTestReturnsTrue(t *testing.T) {
 	if true != false {
 		t.Error("DummyTestReturnsTrue")
